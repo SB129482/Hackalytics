8e7dc37b521b7eabe39c8b33954605ed87010666
James Gao <jamesg@gatech.edu>
2024-02-11 08:53:30 -0500

    refactor to use cloudflare ai

--- stutter-proj/src/App.jsx
+++ stutter-proj/src/App.jsx
@@ -69,58 +69,46 @@ function App() {
     setIsTyping(true);
 
     // process message to chatgpt/cloudflare (send it over and see the response)
-    await processMessageToChatGPT(newMessages);
+    await processMessageToCloudflareAI(newMessages);
   };
 
-  async function processMessageToChatGPT(chatMessages) {
-    // messages is an array of messages
-    // Format messages for chatGPT API
-    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-    // So we need to reformat
-
-    let apiMessages = chatMessages.map((messageObject) => {
-      let role = "";
-      if (messageObject.sender === "ChatGPT") {
-        role = "assistant";
-      } else {
-        role = "user";
+  async function processMessageToCloudflareAI(chatMessages) {
+    const source = new EventSource(
+      "https://stutstut.jamesgaowenzhen.workers.dev?request=" +
+        chatMessages[1].message
+    );
+    let res = "";
+    source.onmessage = (event) => {
+      if (event.data == "[DONE]") {
+        // SSE spec says the connection is restarted
+        // if we don't explicitly close it
+        source.close();
+        setIsTyping(false);
+        return;
       }
-      return { role: role, content: messageObject.message };
-    });
+      const data = JSON.parse(event.data);
 
-    // Get the request body set up with the model we plan to use
-    // and the messages which we formatted above. We add a system message in the front to'
-    // determine how we want chatGPT to act.
-    const apiRequestBody = {
-      model: "gpt-4",
-      messages: [
-        systemMessage, // The system message DEFINES the logic of our chatGPT
-        ...apiMessages, // The messages from our chat with ChatGPT
-      ],
-    };
-
-    await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " + API_KEY,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(apiRequestBody),
-    })
-      .then((data) => {
-        return data.json();
-      })
-      .then((data) => {
-        console.log(data);
+      console.log(data);
+      res += data.response;
+      if (res == "") {
         setMessages([
           ...chatMessages,
           {
-            message: data.choices[0].message.content,
+            message: res,
             sender: "ChatGPT",
           },
         ]);
-        setIsTyping(false);
-      });
+      } else {
+        setMessages([
+          ...chatMessages,
+          {
+            message: res,
+            sender: "ChatGPT",
+          },
+        ]);
+      }
+      // el.innerHTML += data.response;
+    };
   }
 
   const restartFunc = () => {
